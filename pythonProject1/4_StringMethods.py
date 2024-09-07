72c65ba4c0f874e600886187e9cc395c1f8f1542
Bhavik <bgbhimani1395@gmail.com>
2024-09-07 22:51:40 +0530

    Update Readme and add tipmestamp

---
+++ pythonProject1/4_StringMethods.py
@@ -0,0 +1,12 @@
+name = ("bhAvik")
+
+# print(len(name))
+# print(name.find("a"))    # 2 where is first it index
+# print(name.capitalize()) # Bhavik
+# print(name.upper())      # BHAVIK
+# print(name.lower())      # bhavik
+# print(name.isdigit())    # False
+# print(name.isalpha())    # True
+# print(name.isalnum())    # True
+# print(name.replace("b","The B"))  # The BhAvik
+# print((name + " ")*5)   #bhAvik bhAvik bhAvik bhAvik bhAvik
