f4af978184280d978dd955f52a0312a4a78451e9
Bhavik Bhimani <166936122+bgbhimani@users.noreply.github.com>
2024-09-09 12:16:21 +0530

    Update 6_ArithAndMath.py

--- pythonProject1/6_ArithAndMath.py
+++ pythonProject1/6_ArithAndMath.py
@@ -24,10 +24,10 @@ x = 3.14
 y =4
 z = 5
 
-result = round(x) #round the num 3.14->3  3.65->4 
-result = round(x,2) #roud to 2 digits  
-result = abs(y) # Distance from 0
-result = pow(y,3)  # 4^3 = 64
+result = round(x)    # round the num 3.14->3  3.65->4 
+result = round(x,2)  # roud to 2 digits  
+result = abs(y)      # Distance from 0
+result = pow(y,3)    # 4^3 = 64
 result = max(x,y,z)  # max of these
 result = min(x,y,z)  # min of these
 print(result)
