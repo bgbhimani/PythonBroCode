e2733c9cd2cad56a33fa30124e7a83e6c8f45131
Bhavik Bhimani <166936122+bgbhimani@users.noreply.github.com>
2024-09-09 17:45:59 +0530

    Update 13.1_nameValidation.py

--- pythonProject1/13.1_nameValidation.py
+++ pythonProject1/13.1_nameValidation.py
@@ -4,8 +4,7 @@ username = input("Enter Your Username: ")
 if len(username) < 12 and username.find(" ") and username.isalpha():
     print(f"You're In. {username}")
 elif len(username)>12:
-    print("Length must < 12")
-elif username.find(" ") > 0:
+    print("Length must < 12")1  1
     print("No space allowed")
 elif not username.isalpha():
     print("No digit allowed")
