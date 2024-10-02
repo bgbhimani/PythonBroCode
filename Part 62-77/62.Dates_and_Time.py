f27cad61a0afa123a3b106ac667658b320dcbbc6
Bhavik Bhimani <166936122+bgbhimani@users.noreply.github.com>
2024-10-02 12:40:53 +0530

    add folder 62 & 63

--- Part 62-77/62.Dates_and_Time.py
+++ Part 62-77/62.Dates_and_Time.py
@@ -1,4 +1,25 @@
 import datetime as dt
 
-date = dt.date(2025,1,2)
-print(date)
+# date = dt.date(2025,1,2)
+# today = dt.date.today()
+# print("Today =",today)
+# print(date)
+
+time = dt.time(12,3,45) 
+print(time)
+now = dt.datetime.now()
+print(now)
+
+# String formation of time 
+now = now.strftime("%H:%M:%S %d-%m-%y")
+print(now)
+
+# See if target datetime is passed the datetime or not
+target_datetime = dt.datetime(2025,9,13,12,11,15)
+current_datetime = dt.datetime.now()
+
+if target_datetime < current_datetime:
+    print("The targeted Date Passed")
+else:
+    print("Your Targeted Date is not Passed")
+    
---
+++ Part 62-77/63_Alarm_Clock/63.Alarm_Clock.py
@@ -0,0 +1,31 @@
+import time     # for updating time
+import datetime # for String formatation
+import pygame   # for sound
+
+def set_alarm(alarm_time):
+    print(f"alarm set for {alarm_time}")
+    sound_file = "alarm_clock_.mp3"
+    
+    is_running = True
+    while is_running:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+        
+        if current_time == alarm_time:
+            print(":::::Wake Up:::::")
+            
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)     # it will Load the file
+            pygame.mixer.music.play()                        # it will play the file
+            
+            while pygame.mixer.music.get_busy():
+                time.sleep(1)
+                
+            is_running = False
+                
+        time.sleep(1)
+        
+
+if __name__ == "__main__":
+    alarm_time = input("Enter the Alarm time (HH:MM:SS): ")
+    set_alarm(alarm_time)
